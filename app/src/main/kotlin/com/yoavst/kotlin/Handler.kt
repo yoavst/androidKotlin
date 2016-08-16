0986149f40cfba244e0ab12fb7b62bf99804af5b
hubojane <hubojane@naver.com>
2016-08-16 22:06:06 +0900

    fix 'deylaed' to 'delayed'

--- app/src/main/kotlin/com/yoavst/kotlin/Handler.kt
+++ app/src/main/kotlin/com/yoavst/kotlin/Handler.kt
@@ -6,7 +6,7 @@ import android.os.Message
 public fun Handler.post(action: () -> Unit): Boolean = post(Runnable(action))
 public fun Handler.atFrontOfQueue(action: () -> Unit): Boolean = postAtFrontOfQueue(Runnable(action))
 public fun Handler.atTime(uptimeMillis: Long, action: () -> Unit): Boolean = postAtTime(Runnable(action), uptimeMillis)
-public fun Handler.deylaed(delayMillis: Long, action: () -> Unit): Boolean = postDelayed(Runnable(action), delayMillis)
+public fun Handler.delayed(delayMillis: Long, action: () -> Unit): Boolean = postDelayed(Runnable(action), delayMillis)
 public fun handler(handleMessage: (Message) -> Boolean): Handler {
     return android.os.Handler { p0 -> if (p0 == null) false else handleMessage(p0) }
 }
